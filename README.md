293eb4a188f4465929e4ff03154d273d3d100b14
Chulho Yoon <ychidra@gmail.com>
2019-02-21 16:24:50 -0600

    added qunit test

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # module-three-problem
 Module 3 P3
+
+## Source Credits
+- background image: Chulho Yoon
+- Introduction image: Chulho Yoon
+- Facebook icon: [Create.net](https://www.create.net/blog/free-social-media-icons#facebook)
+- Weather widget: [weatherWidget](https://weatherwidget.io/)
+- Video (Youtube): [CPAStrengthYoutube](https://www.youtube.com/watch?v=0--jJn6zqfg)
+- Navbar: [Bootstrap4CDN](https://getbootstrap.com/)
+- Table: [HTMLTableGenerator](https://www.tablesgenerator.com/html_tables)
+- Simpsons Quote API: [theSimpsonsQuoteAPI](https://thesimpsonsquoteapi.glitch.me/)
+- Fetching Data (AJAX) - Studied from: [DEVBeginnersGuidtoFetchingAJAX](https://dev.to/bjhaid_93/beginners-guide-to-fetching-data-with-ajax-fetch-api--asyncawait-3m1l)
+- qUnit Testing: [qunitjs](https://qunitjs.com/)
--- script/yoonjs.js
+++ script/yoonjs.js
@@ -4,57 +4,71 @@ console.log('Declare testable functions')
 function subtract(x, y) {
     return y - x
 }
+function negative(n, m) {
+    let s = m - n;
+    if (s > 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+window.onload = function () {
+    QUnit.test("UnitTest", function (assert) {
+        let subtraction = subtract(1, 2);
+        assert.equal(subtraction, "1", "2 - 1 is 1, so passed!")
+        assert.notEqual(subtraction, "2", "it's 1 so passed!")
+        assert.notEqual(subtraction, "0", "it's 1 so passed!")
+        assert.ok(negative(1, 2), "positive, so passed!")
+        assert.ok(negative(1989, 2019), "positive, so passed!")
+    
+    })
+
+}
 
 console.log('Define constants')
 
 console.log('Declare event listeners')
-
+// simple age calculation
 document.getElementById('clicker').addEventListener('click', function () {
     const n = document.getElementById('name').value
     const i = parseInt(document.getElementById('birthyear').value)
     const j = 2019
+    if (i > 2019) {
+        alert("You are not even born yet!")
+    }
     const ans = n + ', you are ' + subtract(i, j) + ' years old.'
-    document.getElementById('result').innerHTML = ans
+    document.getElementById('result').innerHTML = ans //DOM modification
 
-    console.log('add to local storage')
+    console.log('add to local storage') //local storage addition
     localStorage.setItem("age", ans)
 })
 
 console.log('retrieve previous result')
 document.getElementById('retrieve').addEventListener('click', function () {
-    document.getElementById('retrieving').innerHTML = localStorage.getItem("age")
+    document.getElementById('retrieving').innerHTML = localStorage.getItem("age") //retrieving local storage
 })
 
 console.log('clear local storage')
 document.getElementById('clearall').addEventListener('click', function () {
-    localStorage.clear()
+    localStorage.clear() //clear local storage
     document.getElementById('retrieving').innerHTML = ""
 })
 
-console.log('get random quotes: AJAX')
+console.log('get random quotes: AJAX fetch')
+
 const url = "https://thesimpsonsquoteapi.glitch.me/quotes"
-
-function getJoke() {
-    return new Promise(function (resolve, reject) {  // a promise is an object with logic if resolved or if rejected
-        const req = new XMLHttpRequest();  // make a request - built into JavaScript language
-        req.timeout = 2000; // millisecs to respond
-        req.onreadystatechange = function () {
-            if (req.readyState === 4) {
-                if (req.status === 200) {
-                    const obj = JSON.parse(req.response);
-                    const joke = obj.value.joke;
-                    resolve(joke)
-                } else {
-                    reject(req.status)
-                }
-            }
-        }
-        req.ontimeout = function () {
-            reject('Error - timed out: ' + req.time)
-        }
-        req.open("GET", url, true);  // yes, async
-        req.send();
+fetch(url) //fetch is the simplifed version of xmlhttprequest
+    .then((res) => { return res.json() }) //receiving returning response as JSON application
+    .then((data) => {            // selecting which data retrieved from returned response to be displayed on html
+        data.forEach((item) => {
+            const { quote, character } = item
+            const result = `Quote : "${quote}"  By : ${character}`
+            document.getElementById('simpsons').innerHTML = result;
+        });
     })
-}
+    .catch(function (error) {
+        console.log(JSON.stringify(error)); //logs in console whenever HTTP response error occurs (failed HTTP response handling)
+    })
 
-    console.log('Script End')
+console.log('Script End')
