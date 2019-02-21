80e77c2f6f6416055ec5212db5963ed84261f9a5
Chulho Yoon <ychidra@gmail.com>
2019-02-21 16:52:35 -0600

    improved few things

--- script/yoonjs.js
+++ script/yoonjs.js
@@ -58,7 +58,8 @@ document.getElementById('clearall').addEventListener('click', function () {
 console.log('get random quotes: AJAX fetch')
 
 const url = "https://thesimpsonsquoteapi.glitch.me/quotes"
-fetch(url) //fetch is the simplifed version of xmlhttprequest
+document.getElementById('simpbutt').addEventListener('mouseover', function() {
+    fetch(url) //fetch is the simplifed version of xmlhttprequest
     .then((res) => { return res.json() }) //receiving returning response as JSON application
     .then((data) => {            // selecting which data retrieved from returned response to be displayed on html
         data.forEach((item) => {
@@ -70,5 +71,5 @@ fetch(url) //fetch is the simplifed version of xmlhttprequest
     .catch(function (error) {
         console.log(JSON.stringify(error)); //logs in console whenever HTTP response error occurs (failed HTTP response handling)
     })
-
+})
 console.log('Script End')
